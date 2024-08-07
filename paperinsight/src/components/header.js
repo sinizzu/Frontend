138d7ca38b4b8672c64bbed1183384de1c166d0a
sinzng <tlswlgus3547@duksung.ac.kr>
2024-08-07 08:29:38 +0000

    Fix: search 페이지에서 file name 제거

--- paperinsight/src/components/header.js
+++ paperinsight/src/components/header.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { Box, AppBar, Toolbar, CssBaseline, Typography } from '@mui/material';
-import {Link} from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function Header({ fileName }) {
+
+  const location = useLocation();
   return (
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
@@ -18,7 +20,7 @@ function Header({ fileName }) {
           <img src="/header.png" alt="Header Logo" style={{ height: '40px' }} /></Link>
           <Typography variant="h6" mx={2} noWrap sx={{ color: 'black', fontWeight: 'bold' }}>PDFast</Typography>
           </Box>
-          {fileName && (
+          {fileName && location.pathname !== '/search' && (
             <Typography variant="subtitle1" noWrap sx={{ color: 'black' }}>
               {fileName}
             </Typography>
